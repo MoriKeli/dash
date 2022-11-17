6fa22081c2e27a5d494567e435fbef97591ce3f1
Mori Keli <kelimbth@gmail.com>
2022-11-17 05:59:27 +0300

    configured bar chart

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-
-
 app = dash.Dash()
-
 # application layout
 app.layout = html.Div([
-    html.h1('First dash App!'),
-    html.Div('Dash - A data product development framework from plotly')
+    html.H1('First dash App!'),
+    html.Div('Dash - A data product development framework from plotly'),
+
+    dcc.Graph(
+        id = 'sample-graph',
+        figure = {
+            'data': [
+                {
+                    'x': [1, 2, 3, 4, 5],
+                    'y': [10, 20, 30, 40, 50, 60],
+                    'type': 'bar',
+                    'name': 'Simple bar chart'
+                }    
+            ],
+            'layout': {
+                'title': 'Simple bar chart',
+            }
+
+        },
+    )
 
 ])
-
-
 if __name__ == '__main__':
     app.run_server(debug=True)
