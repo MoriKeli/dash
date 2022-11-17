a3d2cca034b625bdc0660939c755e9bdef8cb389
Mori Keli <kelimbth@gmail.com>
2022-11-17 06:07:27 +0300

    added a 2nd bar

--- main.py
+++ main.py
@@ -5,19 +5,38 @@ import dash_html_components as html
 app = dash.Dash()
 # application layout
 app.layout = html.Div([
-    html.H1('First dash App!'),
-    html.Div('Dash - A data product development framework from plotly'),
+    html.H1(
+        children='First dash App!',
+        style= {
+            'textAlign': 'center',
+            'color': '#456FBV'
+        }
+    
+    ),
+    html.Div(
+        children='Dash - A data product development framework from plotly',
+        style= {
+            'textAlign': 'center',
+            'color': '#BBFGT'
+        }
+    ),
 
     dcc.Graph(
         id = 'sample-graph',
         figure = {
             'data': [
                 {
-                    'x': [1, 2, 3, 4, 5],
-                    'y': [10, 20, 30, 40, 50, 60],
+                    'x': [1, 2, 3, 4, 5, 6],
+                    'y': [10, 20, 30, 40, 50, 120],
                     'type': 'bar',
-                    'name': 'Simple bar chart'
-                }    
+                    'name': 'First bar'
+                },
+                {
+                    'x': [1, 2, 3, 4, 5, 6],
+                    'y': [50, 5, 80, 100, 20, 35],
+                    'type': 'bar',
+                    'name': 'Second bar'
+                }     
             ],
             'layout': {
                 'title': 'Simple bar chart',
